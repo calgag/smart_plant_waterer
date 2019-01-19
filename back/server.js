41be3c83fad532f07557820ae8853fc3ec12b0d7
calgag <calgagi@gmail.com>
2019-01-19 14:07:51 -0800

    testing...

--- back/server.js
+++ back/server.js
@@ -1,8 +1,26 @@
 var express = require("express");
+var gpio = require("gpio");
+
 var app = express();
 
-app.get('/', function(req, res){
+var gpio2 = gpio.export(2, {
+	direction: gpio.DIRECTION.OUT,
+	interval: 200,
+	ready: function(){
+			console.log("Ready");
+	}
+});
+
+app.get("/", function(req, res){
 	res.send("Hello world");
-})
+});
+
+app.get("/on", function(req, res){
+	gpio.set();
+	setTimeout(function() {
+		console.log("waiting...");
+	}, 1000);
+	gpio.set(0);
+});
 
 app.listen(8000);
