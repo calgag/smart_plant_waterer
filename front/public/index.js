36d5e00fed971fbb869d4ac020a608923cd099f4
calgag <calgagi@gmail.com>
2019-01-19 21:08:43 -0800

    button changes based on status of valve

--- front/public/index.js
+++ front/public/index.js
@@ -2,10 +2,14 @@ var app = new Vue({
   el: '#app',
   data: {
     title: "Smart Plant Waterer",
-    buttonNum: [{text: "1"}, {text: "2"}, {text: "3"}, {text: "4"}],
-    openValve: function(valveNum){
+    buttonNum: [{text: "1", open: false}, {text: "2", open: false}, {text: "3", open:false}, {text: "4", open:false}],
+    changeValve: function(num){
       axios.get('https://api.coindesk.com/v1/bpi/currentprice.json').then(
-        response => (console.log(response)));
+        function(response){
+          console.log(response);
+          num.open = !num.open;
+        }
+      );
     }
   }
 });
