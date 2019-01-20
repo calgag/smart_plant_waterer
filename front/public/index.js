6d6689e0337b0ea77225445382f16c3c8799f501
calgag <calgagi@gmail.com>
2019-01-19 20:57:33 -0800

    basically done w/ frontend

--- front/public/index.js
+++ front/public/index.js
@@ -1,7 +1,11 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!',
-    buttonNum: [{text: "1"}, {text: "2"}, {text: "3"}, {text: "4"}]
+    title: "Smart Plant Waterer",
+    buttonNum: [{text: "1"}, {text: "2"}, {text: "3"}, {text: "4"}],
+    openValve: function(valveNum){
+      axios.get('https://api.coindesk.com/v1/bpi/currentprice.json').then(
+        response => (console.log(response)));
+    }
   }
-})
+});
