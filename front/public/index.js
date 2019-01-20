ea8a88242ace84afbd17c66acd382018b43ffca6
calgag <calgagi@gmail.com>
2019-01-20 12:09:51 -0800

    front end working

--- front/public/index.js
+++ front/public/index.js
@@ -4,12 +4,20 @@ var app = new Vue({
     title: "Smart Plant Waterer",
     buttonNum: [{text: "1", open: false}, {text: "2", open: false}, {text: "3", open:false}, {text: "4", open:false}],
     changeValve: function(num){
-      axios.get('https://api.coindesk.com/v1/bpi/currentprice.json').then(
-        function(response){
-          console.log(response);
-          num.open = !num.open;
-        }
-      );
+      if(num.open == false){
+        axios.get("http://10.16.8.92:8000/open/" + num.text).then(
+          function(response){
+            console.log(response);
+          }
+        );
+      }else{
+        axios.get("http://10.16.8.92:8000/close/" + num.text).then(
+          function(response){
+            console.log(response);
+          }
+        );
+      }
+      num.open = !num.open;
     }
   }
 });
